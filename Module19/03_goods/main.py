6bd7ab82a621e03b62fa730322e397bc8a231def
Nikita Fomin <fominquant@yandex.ru>
2024-02-02 16:39:45 +0300

    Module 19 homework passed

--- Module19/03_goods/main.py
+++ Module19/03_goods/main.py
@@ -24,24 +24,13 @@ store = {
     ],
 }
 
-# TODO:
-#  - Идём циклом for по ключам словаря, т.е. по goods.keys(). Переменная цикла - i_name. В цикле:
-#      - Объявляем переменную количества товаров total_quantity, равную 0
-#      - Объявляем переменную суммы товаров total_cost, равную 0
-#      - Идём циклом for по store[goods[i_name]] (Т.е. по получаем значение в goods, лежащее по ключу i_name,
-#      это будет айди. А затем получаем значение из store по ключу, которым является айди полученный ранее.
-#      Тем самым мы получаем нужный список с товарами). Переменная цикла - j_good. В цикле:
-#          - total_quantity увеличиваем на j_good["quantity"]
-#          (т.е. получаем у текущего объекта его кол-во по ключу "quantity")
-#          - total_cost увеличиваем на произведение j_good["price"] и j_good["quantity"]
-#          (т.е. получаем у текущего объекта его кол-во по ключу "quantity", также получаем цену текущего объекта
-#          и перемножаем их)
-#      - Печатаем шаблон "{} - {} шт, стоимость {} руб", в котором через .format() подставляем
-#      переменные i_name, total_quantity и total_cost
+
 for i_name in goods.keys():
     total_quantity = 0
     total_cost = 0
     for j_good in store[goods[i_name]]:
-        total_quantity += j_good['quantity']
-        total_cost += j_good['price'] * j_good['quantity']
-    print('{0} - {1} шт, стоимость {2} руб'.format(i_name, total_quantity, total_cost))
+        total_quantity += j_good["quantity"]
+        total_cost += j_good["price"] * j_good["quantity"]
+    print("{0} - {1} шт, стоимость {2} руб".format(i_name, total_quantity, total_cost))
+
+# зачтено
